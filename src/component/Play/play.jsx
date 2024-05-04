ab0d273572810546d9834fd7525e85af84ebb474
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2024-05-04 17:13:31 +0700

    Navbar
    Navbar

---
+++ src/component/Play/play.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function play() {
+  return (
+    <div>
+      <h2>play</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
+        reprehenderit voluptas nihil praesentium est, enim nulla laboriosam
+        repellendus inventore cum rerum repudiandae odio distinctio ipsam ipsa
+        eum. Porro, quaerat vitae.
+      </p>
+    </div>
+  );
+}
+
+export default play;
