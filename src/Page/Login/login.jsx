608db754fbf7d7313a16e67e0bc0e2296d79e63b
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2024-05-05 23:12:02 +0700

    Login
    -Login

--- src/Page/Login/login.jsx
+++ src/Page/Login/login.jsx
@@ -1,37 +1,48 @@
 import React from "react";
+import Logo from "../../assets/Home-Logo.png";
 
 export default function login() {
   return (
-    <div className="flex justify-center bg-[#FBF3D5] border rounded-md w-[800px] h-[350px] ">
-      <div className="grid grid-cols-2 ">
-        <div className="">
-          <h1 className="py-5">Login</h1>
-          <h2 className="">Does'n have an acoount yet?</h2>
-          <div className="mt-2 ">
-            <div>
-              <label className="text-lg">Email</label>
-              <input
-                className="flex justify-center border rounded-md border-gray-400 h-8 w-60 px-2"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div>
-              <label className="text-lg">Password</label>
-              <input
-                className="flex justify-center border rounded-md border-gray-400 h-8 w-60 px-2"
-                placeholder="Enter your password"
-                type="password"
-              />
-            </div>
-            <div className="py-5">
-              <button className="rounded-full bg-[#FB6D48] px-10 py-2 text-white">
-                Log in
+    <div className="flex justify-center ">
+      <div className="rounded-none border-2 border-gray-400 mt-20">
+        <div className="grid grid-cols-2 ">
+          <div className="px-10 py-10 ">
+            <h1 className="text-3xl font-semibold">Login</h1>
+            <h2 className="font-medium text-lg text-gray-500 mt-4">
+              Does'n have an acoount yet? Sign Up
+            </h2>
+            <div className="mt-8">
+              <div>
+                <label className="text-lg font-medium">Email</label>
+                <input
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
+                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div>
+                <label className="text-lg font-medium">Password</label>
+                <input
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
+                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <button className="mt-3 font-medium text-base text-violet-500">
+                Forget password
               </button>
+              <div className="py-5">
+                <button className="rounded-full bg-[#FB6D48] px-10 py-2 text-white">
+                  Log in
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center">
-          <h2>6666</h2>
+          <div className="flex justify-center bg-gradient-to-r from-[#FFEAA7] to-[#FB6D48] ">
+            <div className="flex items-center">
+              <img src={Logo} alt="" className="w-[300px]" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
