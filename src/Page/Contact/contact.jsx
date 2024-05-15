3806552f8be838b43ce667c2d25ec26b4874f905
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2024-05-15 23:50:39 +0700

    Update contact.jsx

--- src/Page/Contact/contact.jsx
+++ src/Page/Contact/contact.jsx
@@ -2,12 +2,7 @@ const contact = () => {
   return (
     <div>
       <h2>contatc</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
-        reprehenderit voluptas nihil praesentium est, enim nulla laboriosam
-        repellendus inventore cum rerum repudiandae odio distinctio ipsam ipsa
-        eum. Porro, quaerat vitae.
-      </p>
+      <p>55555555555</p>
     </div>
   );
 };
