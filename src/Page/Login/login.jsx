caa12ff10632955f21cf66b35a2de2f7d929218c
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2024-05-27 23:31:36 +0700

    Login

--- src/Page/Login/login.jsx
+++ src/Page/Login/login.jsx
@@ -1,5 +1,5 @@
 import react from "react";
-import Logo from "../../assets/Home-Logo.png";
+import Log from "../../assets/login.png";
 import { AxiosLib } from "../../lib/axiosLib";
 
 const login = () => {
@@ -28,7 +28,75 @@ const login = () => {
       onSubmit={onFormSubmit}
       className="flex justify-center "
     >
-      <div className="rounded-md border-2 shadow-2xl mt-20 w-[700px]">
+      <div className="grid grid-cols-2 mt-20">
+        <div className="flex justify-center">
+          <div className="flex justify-items-center px-20">
+            <div className="rounded-md border-2 shadow-2xl w-[350px]">
+              <div className="px-5">
+                <h1 className="text-2xl font-semibold font-serif mt-3">
+                  Login
+                </h1>
+                <p className="text-black self-center flex gap-1 mt-4 font-serif">
+                  Doesn&apos;t have an account yet?
+                  <a
+                    className="text-[#FB6D48] underline font-sans "
+                    href="/SignUp"
+                  >
+                    Sign Up
+                  </a>
+                </p>
+                <div className="mt-3">
+                  <div>
+                    <label className="text-sm font-medium font-sans ">
+                      Email
+                    </label>
+                    <input
+                      name="email"
+                      type="email"
+                      className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
+                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                      placeholder="Enter your email"
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <label className="text-sm font-medium font-sans">
+                      Password
+                    </label>
+                    <input
+                      name="password"
+                      type="password"
+                      className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
+                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                      placeholder="Enter your password"
+                    />
+                  </div>
+                  <button className="mt-3 font-medium text-base text-violet-500 font-sans ">
+                    Forget password
+                  </button>
+                  <div className="py-3 mt-2">
+                    <button className="rounded-md bg-[#FB6D48] px-10 py-2 text-white font-sans w-full">
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="flex justify-center items-center">
+            <img src={Log} alt="" className="w-5/6 h-3/5" />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default login;
+
+{
+  /* <div className="rounded-md border-2 shadow-2xl mt-20 w-[700px]">
         <div className="grid grid-cols-2 ">
           <div className="px-10 py-10 ">
             <h1 className="text-2xl font-semibold font-serif">Login</h1>
@@ -77,9 +145,5 @@ const login = () => {
             </div>
           </div>
         </div>
-      </div>
-    </form>
-  );
-};
-
-export default login;
+      </div> */
+}
--- src/Page/SignUp/SignUp.jsx
+++ src/Page/SignUp/SignUp.jsx
@@ -35,7 +35,7 @@ const SignUp = () => {
       <div className="grid grid-cols-2 mt-10">
         <div className="flex justify-center">
           <div className="flex justify-items-center px-20">
-            <div className="rounded-md border-2 shadow-2xl w-[400px]">
+            <div className="rounded-md border-2 shadow-2xl w-[350px]">
               <div className="px-5">
                 <h1 className="text-2xl font-semibold font-serif mt-5">
                   Sign Up
@@ -121,89 +121,6 @@ const SignUp = () => {
           </div>
         </div>
       </div>
-      {/* <div className="rounded-md border-2 shadow-2xl mt-20 ">
-        <div className="grid grid-cols-2">
-          <div className="flex justify-center bg-gradient-to-r from-[#FFEAA7] to-[#FB6D48] px-10 py-10">
-            <div className="flex justify-items-center">
-              <img src={Logo} alt="" className="w-[300px]" />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 mt-8">
-            <div className="px-5">
-              <h1 className="text-2xl font-semibold font-serif">Sign Up</h1>
-              <div className="mt-8">
-                <div>
-                  <label className="text-sm font-medium font-serif">
-                    First Name
-                  </label>
-                  <input
-                    name="firstName"
-                    className="mt-1 block w-full px-5 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
-                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                    placeholder="Enter your Firstname"
-                  />
-                </div>
-                <div className="mt-2">
-                  <label className="text-sm font-medium font-serif">
-                    Email
-                  </label>
-                  <input
-                    name="email"
-                    type="email"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
-                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                    placeholder="Enter your Email"
-                  />
-                </div>
-                <div className="mt-2">
-                  <label className="text-sm font-medium font-serif">
-                    Phone
-                  </label>
-                  <input
-                    name="phone"
-                    type="number"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
-                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                    placeholder="Enter your Phone"
-                  />
-                </div>
-                <div className="py-5 mt-2">
-                  <button className="rounded-full bg-[#FB6D48] px-10 py-2 text-white font-serif">
-                    Sign Up
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="mt-16 px-5">
-                <div>
-                  <label className="text-sm font-medium font-serif">
-                    Last Name
-                  </label>
-                  <input
-                    name="lastName"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
-                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                    placeholder="Enter your Lastname"
-                  />
-                </div>
-                <div className="mt-2">
-                  <label className="text-sm font-medium font-serif">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#FB6D48] focus:ring-1 focus:ring-[#FB6D48]
-                  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                    placeholder="Enter your Password"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </form>
   );
 };
