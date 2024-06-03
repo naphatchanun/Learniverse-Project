9e0d959f68630bbc234c451fa373cdb0785f1aef
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2024-06-03 22:32:53 +0700

    Update contact.jsx

--- src/Page/Contact/contact.jsx
+++ src/Page/Contact/contact.jsx
@@ -3,10 +3,16 @@ const contact = () => {
     <div>
       <h2>contatc</h2>
       <p>
+<<<<<<< Updated upstream
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quasi
         voluptatibus non iste. Recusandae, praesentium tempora maiores quos
         accusantium harum modi itaque voluptas excepturi inventore aliquid.
         Quibusdam aperiam debitis earum?
+=======
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
+        reprehenderit voluptas nihil praesentium est, enim nulla laboriosam
+        repellendus inventore cum rerum repudiandae odio distinctio ipsam ipsa
+>>>>>>> Stashed changes
       </p>
     </div>
   );
