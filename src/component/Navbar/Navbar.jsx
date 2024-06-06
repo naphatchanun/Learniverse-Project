8cbe3b519a846e935a6449260db21dfaa0a53487
หนมจีบ <onopingping@gmail.com>
2024-06-07 03:03:21 +0700

    🐛 fix(app): removing create(bro spell wrong)

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -33,14 +33,6 @@ function Navbar() {
           <ul className="flex items-center gap-[4vw] text-xl">
             <li>
               <Link
-                to="/Creact"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Creact
-              </Link>
-            </li>
-            <li>
-              <Link
                 to="/play"
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
